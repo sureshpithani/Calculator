9f0edc47023ba6273c6a4699e8417ccf3ccb8c9e
sureshpithani <167110804+sureshpithani@users.noreply.github.com>
2024-06-24 17:41:08 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,39 @@
+let input = document.getElementById('ans');
+let buttons = document.querySelectorAll('button');
+
+let string ="";
+let arr= Array.from(buttons);
+arr.forEach(button =>{
+    button.addEventListener('click',(e) => {
+        if(e.target.innerHTML == '='){
+            string = eval(string);
+            input.value = string;
+        }
+        else if(e.target.innerHTML == 'AC'){
+            string="";
+            input.value = string;
+        }
+        else if(e.target.innerHTML =='⌫'){
+            string = string.substring(0,string.length-1);
+            input.value = string;
+        }
+        else if(e.target.innerHTML =='('){
+            string = string+'(';
+            input.value = string;
+        }
+        else if(e.target.innerHTML ==')'){
+            string = string+')';
+            input.value = string;
+        }
+        else if(e.target.innerHTML =='x'){
+            string = string+'*';
+            input.value = string;
+        }
+        else{
+            string = string+e.target.innerHTML;
+            input.value =string;
+        }
+        
+    })
+}
+)
